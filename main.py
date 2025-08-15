48dea11a34ed48f26972d5b3cc6e8fe71c84f716
FlyingFaller <bengsaunders@gmail.com>
2025-08-15 01:17:05 -0700

    You know me, always testing my code in here.

--- main.py
+++ main.py
@@ -43,13 +43,26 @@ if __name__ == "__main__":
     #             discord_printout=True,
     #             max_guesses = 10)
 
-    all_words = get_words(refetch=False)
+    guesses = get_words(refetch=False)
+    answers = get_words(refetch=False)
+    pattern_matrix = get_pattern_matrix(guesses, answers, savefile = "data/full_pattern_matrix.npy")
     past_answers = scrape_words(refetch=True, save=False)
     original_answers = get_words(refetch=False, savefile=ORIGINAL_ANSWERS_FILE)
     positive_words = np.union1d(past_answers, original_answers)
 
-    word_features = get_word_features(all_words=all_words)
-    prediction_func = load_classifier(word_features, positive_words=positive_words, all_words=all_words, retrain=True)
-    filtered_words = filter_words_by_probability(prediction_func, all_words)
-    print(f'{len(all_words) = }')
-    print(f'{len(filtered_words) = }')
+    word_features = get_word_features(all_words=guesses)
+    prediction_func = load_classifier(word_features, positive_words=positive_words, all_words=guesses, retrain=False)
+    reduced_answers = filter_words_by_probability(prediction_func, guesses)
+    print(f'{len(guesses) = }')
+    print(f'{len(reduced_answers) = }')
+    play_wordle(
+        pattern_matrix, 
+        guesses, 
+        original_answers, 
+        nprune_global=10, 
+        nprune_answers=0, 
+        starting_guess=None, 
+        show_stats=True, 
+        discord_printout=False,
+        max_guesses = 10,
+        sort_func=prediction_func)
