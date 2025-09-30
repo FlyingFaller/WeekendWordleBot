4b5ea9f7acb357dc94a0b5e0ec4d246d29a53231
Benjamin Saunders <freezecubed@gmail.com>
2025-09-30 02:18:45 -0700

    Added both gui and cli entry points to this file for debugging

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 # In src/main.py
 
-from weekendwordle.gui.wordle_app import run_app
-# from weekendwordle.backend.main import run
+# from weekendwordle.gui.wordle_app import run_gui
+from weekendwordle.backend.cli_app import run_cli
 
 if __name__ == "__main__":
     print("🚀 Starting Weekend Wordle...")
-    run_app()
-    # run()
+    run_cli()
+    # run_gui()
