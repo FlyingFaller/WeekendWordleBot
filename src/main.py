6e6098db4c7a04417cbca753ff0874abed087c6e
Benjamin Saunders <freezecubed@gmail.com>
2025-09-30 23:29:43 -0700

    Deleted this file it was just for testing

--- src/main.py
+++
@@ -1,9 +0,0 @@
-# In src/main.py
-
-# from weekendwordle.gui.wordle_app import run_gui
-from weekendwordle.backend.cli_app import run_cli
-
-if __name__ == "__main__":
-    print("🚀 Starting Weekend Wordle...")
-    run_cli()
-    # run_gui()
